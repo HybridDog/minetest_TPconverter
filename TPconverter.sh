734de1d3b3bd705ec83b1db34293abb663bc6c7e
HybridDog <adresse_test@t-online.de>
2013-03-21 09:29:23 +0100

    short it again

--- TPconverter.sh
+++ TPconverter.sh
@@ -4,6 +4,7 @@ b=$((s*3))
 c=$((s*5))
 d=$((s*7))
 si=${s}x${s}+
+edt="mogrify -crop ${si}"
 mkdir minetest
 cp terrain.png minetest/obj.png
 cp gui/items.png minetest/items.png
@@ -44,52 +45,52 @@ rm tools.png
 
 echo item textures copied
 
-mogrify -crop ${si}0 default_grass.png
-mogrify -crop ${si}$a default_dirt.png
-mogrify -crop ${si}$b+$s default_gravel.png
-mogrify -crop ${si}0+$s default_cobble.png
-mogrify -crop ${si}$s default_stone.png
-mogrify -crop ${si}$b default_grass_side.png
-mogrify -crop ${si}$b+$a default_bookshelf.png
-mogrify -crop ${si}$a+$s default_sand.png
-mogrify -crop ${si}$d default_brick.png
-mogrify -crop ${si}$((3*a))+$((2*a)) default_cactus_side.png
-mogrify -crop ${si}$c+$((2*a)) default_cactus_top.png
-mogrify -crop ${si}$((2*a)) default_wood.png
-mogrify -crop ${si}$((2*a))+$s default_tree.png
-mogrify -crop ${si}$c+$s default_tree_top.png
-mogrify -crop ${si}$((4*a))+$((2*a)) default_clay.png
-mogrify -crop ${si}$s+$b default_glass.png
-mogrify -crop ${si}$d+$a default_junglegrass.png
-mogrify -crop ${si}$((3*b))+$((3*b)) default_jungletree.png
-mogrify -crop ${si}$((2*a))+$b default_leaves.png
-mogrify -crop ${si}$d+$b default_dry_shrub.png
-mogrify -crop ${si}$a+$a default_mineral_coal.png
-mogrify -crop ${si}$s+$a default_mineral_iron.png
-mogrify -crop ${si}$((2*a))+$a default_mossycobble.png
-mogrify -crop ${si}$((3*b))+$((2*a)) default_papyrus.png
-mogrify -crop ${si}0+$((6*a)) default_sandstone.png
-mogrify -crop ${si}$((5*b)) default_sapling.png
-mogrify -crop ${si}$((2*b))+$s default_steel_block.png
-mogrify -crop ${si}0+$c default_torch_on_floor.png
-mogrify -crop ${si}$((2*d))+$b default_furnace_bottom.png
+${edt}0 default_grass.png
+${edt}$a default_dirt.png
+${edt}$b+$s default_gravel.png
+${edt}0+$s default_cobble.png
+${edt}$s default_stone.png
+${edt}$b default_grass_side.png
+${edt}$b+$a default_bookshelf.png
+${edt}$a+$s default_sand.png
+${edt}$d default_brick.png
+${edt}$((3*a))+$((2*a)) default_cactus_side.png
+${edt}$c+$((2*a)) default_cactus_top.png
+${edt}$((2*a)) default_wood.png
+${edt}$((2*a))+$s default_tree.png
+${edt}$c+$s default_tree_top.png
+${edt}$((4*a))+$((2*a)) default_clay.png
+${edt}$s+$b default_glass.png
+${edt}$d+$a default_junglegrass.png
+${edt}$((3*b))+$((3*b)) default_jungletree.png
+${edt}$((2*a))+$b default_leaves.png
+${edt}$d+$b default_dry_shrub.png
+${edt}$a+$a default_mineral_coal.png
+${edt}$s+$a default_mineral_iron.png
+${edt}$((2*a))+$a default_mossycobble.png
+${edt}$((3*b))+$((2*a)) default_papyrus.png
+${edt}0+$((6*a)) default_sandstone.png
+${edt}$((5*b)) default_sapling.png
+${edt}$((2*b))+$s default_steel_block.png
+${edt}0+$c default_torch_on_floor.png
+${edt}$((2*d))+$b default_furnace_bottom.png
 cp default_furnace_bottom.png default_furnace_top.png
-mogrify -crop ${si}$((4*b))+$a default_furnace_front.png
-mogrify -crop ${si}$((13*s))+$b default_furnace_front_active.png
-mogrify -crop ${si}$((13*s))+$a default_furnace_side.png
-mogrify -crop ${si}$b+$c default_ladder.png
-mogrify -crop ${si}$((11*s))+$s default_chest_front.png
-mogrify -crop ${si}$((2*c))+$s default_chest_side.png
-mogrify -crop ${si}$((3*b))+$s default_chest_top.png
-mogrify -crop ${si}$((2*c))+0 default_apple.png
-mogrify -crop ${si}$((11*s))+$b default_book.png
-mogrify -crop ${si}$((2*b))+$s default_clay_brick.png
-mogrify -crop ${si}$((3*b))+$b default_clay_lump.png
-mogrify -crop ${si}$d default_coal_lump.png
-mogrify -crop ${si}$((2*c))+$b default_paper.png
-mogrify -crop ${si}$d+$s default_steel_ingot.png
-mogrify -crop ${si}$c+$b default_stick.png
-mogrify -crop ${si}$a+$d default_tool_steelaxe.png
-mogrify -crop ${si}$a+$((2*b)) default_tool_steelpick.png
+${edt}$((4*b))+$a default_furnace_front.png
+${edt}$((13*s))+$b default_furnace_front_active.png
+${edt}$((13*s))+$a default_furnace_side.png
+${edt}$b+$c default_ladder.png
+${edt}$((11*s))+$s default_chest_front.png
+${edt}$((2*c))+$s default_chest_side.png
+${edt}$((3*b))+$s default_chest_top.png
+${edt}$((2*c))+0 default_apple.png
+${edt}$((11*s))+$b default_book.png
+${edt}$((2*b))+$s default_clay_brick.png
+${edt}$((3*b))+$b default_clay_lump.png
+${edt}$d default_coal_lump.png
+${edt}$((2*c))+$b default_paper.png
+${edt}$d+$s default_steel_ingot.png
+${edt}$c+$b default_stick.png
+${edt}$a+$d default_tool_steelaxe.png
+${edt}$a+$((2*b)) default_tool_steelpick.png
 
 echo textures edited
